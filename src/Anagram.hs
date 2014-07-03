e21043af35df964460f9151ef2307b4812082dff
Brian Shin <bys8@cornell.edu>
2014-07-03 15:36:44 +0000

    made implementation cleaner

--- src/Anagram.hs
+++ src/Anagram.hs
@@ -4,23 +4,24 @@ module Anagram (anagram) where
     import Control.Monad.List
     import Control.Monad.Identity
 
-    type Ana = StateT (Src, Tar) []
+    type Ana = StateT Src []
     type Src = String
     type Tar = String
 
-    anagramHelper :: Ana Tar
-    anagramHelper = do
-        (src, tar) <- get
+    anagramHelper :: Tar -> Ana Tar
+    anagramHelper tar = do
+        src <- get
         (src', c) <- lift $ pick src
-        put (src', tar ++ [c])
         if isEmpty src'
             then return (tar ++ [c])
-            else anagramHelper
+            else do
+                put src'
+                anagramHelper (tar ++ [c])
 
     isEmpty :: [a] -> Bool
     isEmpty [] = True
     isEmpty _ = False
-    
+
     pick :: Src -> [(Src, Char)]
     pick src = do
         c <-  src
@@ -30,6 +31,5 @@ module Anagram (anagram) where
     removeOne c src = takeWhile (/= c) src ++ tail (dropWhile (/= c) src)
 
     anagram :: String -> [String]
-    anagram src = evalStateT anagramHelper (src,"")
+    anagram src = evalStateT (anagramHelper "") src
 
-    
